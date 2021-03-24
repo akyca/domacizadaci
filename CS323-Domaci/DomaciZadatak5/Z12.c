b10f9cea1b659a0cb1f0fd1bcfec63e726e764d9
akyca <astojanovic321@gmail.com>
2021-03-24 23:36:32 +0100

    aa

--- CS323-Domaci/DomaciZadatak5/Z12.c
+++ CS323-Domaci/DomaciZadatak5/Z12.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
+12. Napisati program kojim se učitava niz struktura deklarisan sa:
+Struct licnost osoba[MAXOS],*pok;
+i ispisuje na dva načina. Prvo korišćenjem elemenata niza, a zatim korišćenjem
+pokazivača koji se inicijalizuje adresom niza struktura. Struktura treba da sadrži
+podatke: char ime[30], char adresa[50], unsigned starost
+*/
+
 void main(){
 
+	if(1 ==1 ){
+
+	}
 }
