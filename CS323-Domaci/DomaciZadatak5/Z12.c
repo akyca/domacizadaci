73dcc50a913cb09c6949c4edb2fe70703e3f2f55
akyca <astojanovic321@gmail.com>
2021-03-25 00:16:38 +0100

    zd12

--- CS323-Domaci/DomaciZadatak5/Z12.c
+++ CS323-Domaci/DomaciZadatak5/Z12.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAXOS 2
+#include <string.h>
 /* 
 12. Napisati program kojim se učitava niz struktura deklarisan sa:
 Struct licnost osoba[MAXOS],*pok;
@@ -9,9 +10,43 @@ pokazivača koji se inicijalizuje adresom niza struktura. Struktura treba da sad
 podatke: char ime[30], char adresa[50], unsigned starost
 */
 
-void main(){
-
-	if(1 ==1 ){
-
+struct licnost {
+	char ime[30];
+	char adresa[50];
+	unsigned starost;
+};
+void ispisiNizom(struct licnost o[]){
+	printf("Ispisivanje nizom\n");
+	for(int i = 0; i < MAXOS;i++){
+		printf("Ime: %s\n",o[i].ime);
+		printf("Adresa: %s\n",o[i].adresa);
+		printf("Starost: %d\n",o[i].starost);
+		printf("\n");
 	}
+
 }
+void ispisiPokazivacem(struct licnost *p){
+	printf("Ispisivanje pokazivacem\n");
+	for(int i =0; i < MAXOS; i++){
+		printf("Ime: %s\n", (p+i)->ime);
+		printf("Adresa: %s\n", (p+i)->adresa);
+		printf("Ime: %d\n", (p+i)->starost);
+		printf("\n");
+	}
+	
+}
+void main(){
+	struct licnost osoba[MAXOS], *pok;
+	strcpy(osoba[0].ime, "andrija");
+	strcpy(osoba[0].adresa, "Dragise Cvetkovica");
+	osoba[0].starost = 20;
+	
+	strcpy(osoba[1].ime, "Jovan");
+	strcpy(osoba[1].adresa , "Visocka");
+	osoba[1].starost = 20;
+	
+	ispisiNizom(osoba);
+	pok = osoba;
+	ispisiPokazivacem(pok);
+}
+
